0d2bd2c524897d0182648dcafbffd044af8518ec
Daniel Mania <danielmania93@gmail.com>
2016-11-25 15:19:19 +0100

    Kernel files added

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#2 SMP PREEMPT Thu Nov 24 20:43:35 CET 2016"
+#define LINUX_COMPILE_BY "daniel"
+#define LINUX_COMPILE_HOST "dadziokpl"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
